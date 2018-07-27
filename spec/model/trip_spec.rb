7c749d1a8ba0d8845de61bb9b8291e0ba7b57d25
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-27 10:42:14 -0600

    Add method for finding longest hiking distance to trip model.

--- spec/model/trip_spec.rb
+++ spec/model/trip_spec.rb
@@ -16,5 +16,12 @@ describe Trip, type: :model do
 
       expect(trip.average_hiking_distance).to eq(7)
     end
+    it '#longest_hiking_distance' do
+      trip = Trip.create(name: 'Test 1', start_date: Date.new(2018, 6, 27), end_date: Date.new(2018, 6, 28))
+      trail_1 = trip.trails.create(name: 'Trail 1', length: 4, address: '123 Main St')
+      trail_2 = trip.trails.create(name: 'Trail 2', length: 10, address: '321 Oak St')
+
+      expect(trip.longest_hiking_distance).to eq(10)
+    end
   end
 end
