31c25ee3f536393ebce27763f706f7071311039e
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-27 10:21:19 -0600

    Add ability for users to click a link from the trips index to go the a specific trip show page.

--- spec/features/user_sees_all_trip_spec.rb
+++ spec/features/user_sees_all_trip_spec.rb
@@ -10,4 +10,14 @@ describe "A user visits '/trips'" do
     expect(page).to have_content(trip_1.name)
     expect(page).to have_content(trip_2.name)
   end
-end 
+  it 'they can view a hiking trip show page by clicking a link' do
+    trip_1 = Trip.create(name: 'Test 1', start_date: Date.new(2018, 6, 27), end_date: Date.new(2018, 6, 28))
+    trip_2 = Trip.create(name: 'Test 2', start_date: Date.new(2018, 5, 27), end_date: Date.new(2018, 5, 28))
+
+    visit trips_path
+
+    click_link trip_1.name
+
+    expect(current_path).to eq(trip_path(trip_1))
+  end
+end
